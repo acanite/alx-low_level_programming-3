30299ef7566018b8a4e0cffc6fbbd4e5fcd13ec3
Ude Perpetual <uperpetual3@gmail.com>
2022-09-08 16:54:19 -0700

    Task 5- printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Return (0) main function
- * return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the overcome is a piece of art, \n");
 	return (0);
 }

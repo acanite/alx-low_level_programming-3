f30e9036191f1ce180a3e1ee1d4c7ec4e646c48b
Ude Perpetual <uperpetual3@gmail.com>
2022-09-08 15:22:41 -0700

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 
 #include <stdio.h>
 /**
- * main - main block
- * return: 0
+ * main - Entry point
+ *
+ * return: Always 0 (Sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-betty 4-puts.c
-betty (sucess)
 

6993b99e1c7c54d6e4881b0de92744c01f6190af
Ude Perpetual <uperpetual3@gmail.com>
2022-09-09 21:58:26 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,11 @@
 #include <time.h>
 /**
  * main - Entry point
- *
  * Return:Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')

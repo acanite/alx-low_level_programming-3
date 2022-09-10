397852cddf1a67e8a9021ece1455731086fb8889
Ude Perpetual <uperpetual3@gmail.com>
2022-09-09 21:54:59 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
@@ -15,7 +15,6 @@ int main(void)
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
-
 	putchar('\n');
 
 	return (0);

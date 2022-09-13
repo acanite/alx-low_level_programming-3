ac24e991413157cfaf5136e428eb53fb1cc725ba
root <uperpetual3@gmail.com>
2022-09-13 14:41:48 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - No entry point
+ *
+ * Return: On success Nothing.
+ */
+
+void print_alphabet_10x(void)
+{
+	char alphabet;
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}

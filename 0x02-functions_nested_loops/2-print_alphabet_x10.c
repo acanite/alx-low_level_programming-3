5a592616955e65460377889cac4d24bf653cf84f
root <uperpetual3@gmail.com>
2022-09-13 15:10:37 -0700

    Refactor correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int i;
 
-	for ( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
 		_putchar('\n');
-	} 
+	}
 }

988c7e443b0202931e779309843b5ca3791619ef
root <uperpetual3@gmail.com>
2022-09-13 16:27:07 -0700

    first attempt

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @n: An integer input
+ * Return: print the sign of n
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return(-1);
+	}
+}

93e96564fb355a5bc259f370d6242b93cdeaa5d0
root <uperpetual3@gmail.com>
2022-09-14 14:45:20 -0700

    Refactor code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,30 +9,20 @@
 
 void print_to_98(int n)
 {
-	int i;
-
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (n = n; n < 98: n++)
 		{
-			printf("%d",i);
-			if (i < 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ",n);
+			printf("%d, ",98);
 		}
-		printf("\n");
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (n = n; n > 98; n--)
 		{
-			printf("%d",i);
-			if (i > 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ",n);
+			printf("%d\n",98);
 		}
-		printf("\n");
 	}
 }

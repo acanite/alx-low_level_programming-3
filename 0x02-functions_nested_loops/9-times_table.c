d7f39b150415a13974dbc602bccadf69f0b56cec
root <uperpetual3@gmail.com>
2022-09-14 15:55:06 -0700

    fixed return 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,17 +14,17 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			res = i * j;
-			if (j !=0)
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (res > 10)
+			if (res >= 10)
 			{
 				_putchar((res / 10) + '0');
 				_putchar((res % 10) + '0');
 			}
-			else if (res < 10 && j !=0)
+			else if (res < 10 && j != 0)
 			{
 				_putchar(' ');
 				_putchar((res % 10) + '0');

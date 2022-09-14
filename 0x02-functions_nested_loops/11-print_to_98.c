01ee263ea71bd865a76886365fdf44774cc81e18
root <uperpetual3@gmail.com>
2022-09-14 15:00:47 -0700

    revert code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,31 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
 	{
-		for (n = n; n < 98; n++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ",n);
-			printf("%d, ",98);
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(", ");
+			}
 		}
+		printf("\n");
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ",n);
-			printf("%d\n",98);
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
+
 		}
+		printf("\n");
 	}
 }

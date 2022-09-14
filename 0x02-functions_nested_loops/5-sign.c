5fee697a583c5c66f22c9ea1240655476c49e8fc
root <uperpetual3@gmail.com>
2022-09-14 12:31:28 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,11 +12,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar ('+');
-		return (1);
+		return (1);i
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		_putchar ('-');
-		return(-1);
-	}
+		_putchar ('0');
+		return (0);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * jack_bauer - check main
+ * Description: Print every minute of the day
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (

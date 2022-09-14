7facc0bc12301376c3a3268811255447bb2d8ac7
root <uperpetual3@gmail.com>
2022-09-14 15:50:09 -0700

    revert code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,31 +7,31 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int res = i * j;
-			if (res <= 9)
+			res = i * j;
+			if (j !=0)
 			{
-				_putchar(res + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			else
+			if (res > 10)
 			{
 				_putchar((res / 10) + '0');
 				_putchar((res % 10) + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			}
+			else if (res < 10 && j !=0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				_putchar((res % 10) + '0');
 			}
 		}
 		_putchar('\n');

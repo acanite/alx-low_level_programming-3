8e40291fe529d2d356833416c76aa143e090f9ae
root <uperpetual3@gmail.com>
2022-09-14 08:10:17 -0700

    first attempt

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,18 @@
 
 /**
  * _islower - check main
- * Return: 0 or 1
+ * Return: 0 or 1.
+ * @i: An integer input
+ * Description: Function that checks lower case
  */
-int _isl 
+int _islower(int i)
+{
+	if (i >= 'a' && i <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
 }

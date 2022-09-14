3966c816b4b9d40d836b642ed97b323c81c854e0
root <uperpetual3@gmail.com>
2022-09-14 15:45:28 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,6 +2,7 @@
 
 /**
  * jack _bauer - function that prints every minute of the day of Jack Bauer
+ * Description: functions that prints every mintue of the day of Jack Bauer
  * starting from 00:00 to 23:59.
  * Return: nothing.
  */

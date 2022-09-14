b67b89c689785ffa8609372fed3742bd02993258
root <uperpetual3@gmail.com>
2022-09-14 08:45:25 -0700

    debug 2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,14 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n >0)
+	if (n > 0)
 	{
 		last = n % 10;
 		_putchar(last + '0');
 	}
 	else
 	{
-		last = -1 * n% 10;
+		last = -1 * n % 10;
 		_putchar(last + '0');
 	}
 	return (last % 10);

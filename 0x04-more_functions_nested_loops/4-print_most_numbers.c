0def8d820a080aa231e3a1a87649548ac6f9e788
root <uperpetual3@gmail.com>
2022-09-15 15:15:36 -0700

    04-print_most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_most_numbers - check main
+ * Description: print numbers 0 to 9
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}

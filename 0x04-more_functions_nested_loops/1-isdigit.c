f502851e05f41c6cf0ed37acff952bc6c1feebc3
root <uperpetual3@gmail.com>
2022-09-15 14:36:41 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - check main 
+ * @c: an integer input
+ * Description: checks for a digit
+ * Return: Boolean
+ */
+
+int _isdigit(int c)
+{
+	char i;
+	int n = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == c)
+		{
+			n = 1;
+			break;
+		}
+	}
+	return (n);
+}

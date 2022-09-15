be840b651a4061e17937a135848e1c99239d95f0
root <uperpetual3@gmail.com>
2022-09-14 17:18:21 -0700

    104- fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main - finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
@@ -14,7 +16,7 @@ int main(void)
 
 	printf("%lu", j);
 
-	for (i = 1, i < 91, i++)
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
 		k = k + j;
@@ -26,7 +28,7 @@ int main(void)
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
 
-	for (i = 92, i < 99, ++i)
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);

e4c45067d741611b01459539f257337b5755fb1e
root <uperpetual3@gmail.com>
2022-09-14 17:20:48 -0700

    change logic

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,4 +27,21 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((res % 10) + '0');
 				}
-				else if(res >= 10 &&res < 100)
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}

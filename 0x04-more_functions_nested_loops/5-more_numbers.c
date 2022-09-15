9582313072e239696a49fa9cef2c6100bbae43aa
root <uperpetual3@gmail.com>
2022-09-15 16:12:58 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,16 +18,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
 		}
-		printf("\n");
+		_putchar("\n");
 	}
 }

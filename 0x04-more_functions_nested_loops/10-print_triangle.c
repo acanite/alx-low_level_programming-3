941aefd4a07782fb8120a34564cb8a40154ad821
root <uperpetual3@gmail.com>
2022-09-15 23:19:30 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,20 @@
  */
 void print_triangle(int size)
 {
-	int i = 0, j, n = size - 1;
+	int i, j;
 
 	if (size > 0)
 	{
-		for (; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = size; j >= 1; j--)
 			{
-				if (j < n)
-					_putchar(' ');
+				if (i < j)
+					-putchar(' ');
 				else
 					_putchar('#');
 			}
-			n--;
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 	else

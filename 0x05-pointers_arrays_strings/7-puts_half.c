4a763fd56d509a2468d41c89890a22d9f8f62bec
root <uperpetual3@gmail.com>
2022-09-19 13:13:52 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,7 +24,7 @@ void puts_half(char *str)
 		}
 	}else if (length % 2)
 	{
-		for (n = (length -1) / 2; n < len - 1; n++)
+		for (n = (length -1) / 2; n < length - 1; n++)
 			_putchar(str[n + 1]);
 	}
 	_putchar('\n');

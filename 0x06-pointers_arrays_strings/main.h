888cece82338a4452f7e1dd2dfa37024e764b183
root <uperpetual3@gmail.com>
2022-09-22 13:36:49 -0700

    main foxed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,8 +7,14 @@ int _putchar(char c);
 void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
+char *_strcmp(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
+char leet(char *);
+char *rot13(char *);
+void print_number(int n);
+
 
 #endif
+

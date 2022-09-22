3322479d2e283765a5d4505daa1db21baf7128ef
root <uperpetual3@gmail.com>
2022-09-22 13:28:11 -0700

    3 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @src: string
+ * @dest: string
+ * Return: edited string
+ * @n: number of char to copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	dest[j] = src[j];for (; n		dest[j] = 
+}'\0';}
+	return (dest);
+	}

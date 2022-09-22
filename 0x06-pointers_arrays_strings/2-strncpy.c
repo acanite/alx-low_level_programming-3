5027901afee585f499f5f6472f12224e27134e02
root <uperpetual3@gmail.com>
2022-09-22 14:32:33 -0700

    fix 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-	dest[j] = src[j];for (; n		dest[j] = 
-}'\0';}
+		dest[j] = src[j];
+	for (; n > i; i++)
+		dest[i] = '\0';
 	return (dest);
-	}
+}

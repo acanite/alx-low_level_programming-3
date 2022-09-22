ffd74de18e99a0f03fb9872fd51db2f9ac094ad7
root <uperpetual3@gmail.com>
2022-09-22 12:18:28 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - function comute strings
+ *@dest: pointing to a char
+ *@src: pointing to a char
+ *Return: value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = 0;
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifdef FILE_MAIN
+#define FILE_MAIN
+
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
+char *_strncat(char *dest, char *src, int n);
+char _strcmp(char *s1, char *s2);
+

dd5196fc76b39c2f605ac28e00e8bc6db69ac355
root <uperpetual3@gmail.com>
2022-09-22 14:43:10 -0700

    first advanced

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number -checkk main
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int ni;
+
+	ni = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		ni = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}

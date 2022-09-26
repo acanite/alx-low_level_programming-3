b1e67943d367a33defaf8e3bbaef0a7b3bfd57d8
root <uperpetual3@gmail.com>
2022-09-26 15:21:26 -0700

    8-print_diagsums

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - function that sets the value of a pointer to a char.
+ *@s: source for copy
+ *@to: string to copy to.
+ *Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

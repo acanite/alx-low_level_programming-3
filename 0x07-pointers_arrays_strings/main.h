879adaa8abad1a9951e6717d0eef1a6476261252
root <uperpetual3@gmail.com>
2022-09-26 11:24:40 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-char *_memset(char *S, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s char c);
 char *_strpbrk(char *s, char *accept);

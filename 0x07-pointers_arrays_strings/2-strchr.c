98611b8653a5ed6f803a3f155cc66f89e480a459
root <uperpetual3@gmail.com>
2022-09-26 12:39:08 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates character in string
+ * @s: string
+ * @c: Character to search
+ * Return: Pointer to first instance of character
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		++s
+	}
+	if (*s == c)
+		return (s);
+	return (0);
+}

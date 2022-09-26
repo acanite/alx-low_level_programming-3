9b900f89681c88c92104ede3267c725ef5188211
root <uperpetual3@gmail.com>
2022-09-26 11:38:23 -0700

    fix main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stddef.h>
-#include <stdio.h>
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s char c);
 char *_strpbrk(char *s, char *accept);
 char *strstr(char *haystack, char *needle);
-
-#endif 

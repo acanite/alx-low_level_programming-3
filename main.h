4883b4e596d6869b95e37903ad435de9dcb104c3
root <uperpetual3@gmail.com>
2022-10-06 04:38:07 -0700

    checked.c

--- main.h
+++ main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#defind MAIN_H
+
+void _puts(char *str);
+
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -7,3 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int _putchar(char c);
+
+int atoi(const char *s);
+
+void _print_int(unsigned long int n);
+
+
+#endif

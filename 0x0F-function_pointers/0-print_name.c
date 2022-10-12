a71e33c8bee44a38a012c105ba6db987544b0b57
root <uperpetual3@gmail.com>
2022-10-12 14:18:03 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,11 @@
 /**
  * print_name - Prints a name
  * @name: The name to prints
- * @f: Pointer to function
- *
- * Return: Nothing
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }

8e1cf8d60ae915fe6a13a75bddabac97084a0565
root <uperpetual3@gmail.com>
2022-10-12 14:38:04 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function given as a 
+ * array_iterator - Executes a function given as a
  * parameter on each element of an array.
  * @array: The array.
  * @size: The size of array.

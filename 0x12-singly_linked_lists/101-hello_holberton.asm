70a763a498c71309ee301461ed7ecbceff1c1a6e
root <uperpetual3@gmail.com>
2022-10-20 07:48:10 -0700

    task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
-section .text
-global main
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	section .text
+	global main
+	extern printf
 
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, msgleh
-	syscall
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
-
-section .rodata
-	msg: db "Hello, Holberton", 10
-	msglen: equ $ - msg
+	mov eax, 0
+	ret

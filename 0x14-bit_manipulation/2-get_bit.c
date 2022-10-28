80ddd78e56af4ae0dde553a41f0c9aa63de59658
root <uperpetual3@gmail.com>
2022-10-28 14:28:39 -0700

    refactor

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int bit;
+
+	if (index > 64)
 		return (-1);
 
-
-	if ((n & (1 << index)) == 0)
-		return (0);
-
-	return (1);
+	bit = n >> index;
+	return (bit & 0x1);
 }

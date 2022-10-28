c6bfa37ee13cc59faa48c6f0a974147e17c414c5
root <uperpetual3@gmail.com>
2022-10-28 14:37:26 -0700

    mandatory done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	int len = 0;
 
-	if (b[len] =='\0')
+	if (b[len] == '\0')
 		return (0);
 
 	while ((b[len] == '0') || (b[len] == '1'))
